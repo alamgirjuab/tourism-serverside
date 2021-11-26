b45207e2fa1f07188d9ff86f3f4d89072e0aecd4
alamgirjuab <alamgirjuab@gmail.com>
2021-11-26 09:26:31 +0600

    comment changed

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ async function run() {
             res.send(services);
         });
 
-        // GET Single Service
+        // GET SINGLE SERVICE
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             console.log('getting specific service', id);
